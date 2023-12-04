c3feb9fdfb212520929358028568fa2d3213edf8
DESKTOP-57RAB5D\arthi <arthipsrain@gmail.com>
2023-12-04 19:28:12 +0700

    test

---
+++ FontEnd/src/app/product-s/product-s.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-s',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-s.component.html',
+  styleUrl: './product-s.component.scss'
+})
+export class ProductSComponent {
+
+}
